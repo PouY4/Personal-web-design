6edd6345919851f830ae783ebbc9dc45c8334714
Pooya Baghbani <pooyabaghbani@gmail.com>
2024-07-25 22:29:26 +0330

    Design using htm css and js

---
+++ Scripts/app.js
@@ -0,0 +1,26 @@
+const navToggleIcon = document.querySelector('.nav__toggle-icon')
+const menu = document.querySelector('.menu')
+const cover = document.querySelector('.cover')
+const resumeListItems = document.querySelectorAll('.resume-list__item')
+const portfolioListItems = document.querySelectorAll('.portfolio-list__item')
+
+navToggleIcon.addEventListener('click', function(){
+    this.classList.toggle('nav__toggle-icon--open')
+    menu.classList.toggle('menu--open')
+    cover.classList.toggle('cover--show')
+})
+
+function navigationTabsInit(listItems, listItemActiveClass, contentItemShowClass){
+    listItems.forEach(listItem => {
+        listItem.addEventListener('click', function(){
+            document.querySelector(`.${listItemActiveClass}`).classList.remove(listItemActiveClass)
+            document.querySelector(`.${contentItemShowClass}`).classList.remove(contentItemShowClass)
+            this.classList.add(listItemActiveClass)
+            let contentId = this.getAttribute('data-content-id')
+            document.querySelector(contentId).classList.add(contentItemShowClass)
+        })
+    })
+}
+
+navigationTabsInit(resumeListItems, 'resume-list__item--active', 'resume-content--show')
+navigationTabsInit(portfolioListItems, 'portfolio-list__item--active', 'portfolio-content--active')
